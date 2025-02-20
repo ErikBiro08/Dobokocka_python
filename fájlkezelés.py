74abfa36a42256e5c40140edaa35d414a2396bbd
GalambosBalint <152618064+GalambosBalint@users.noreply.github.com>
2025-02-20 09:14:20 +0100

    fájlkezelés
    Ez a fájlkezelés fájl amit én csináltam.

---
+++ fájlkezelés.py
@@ -0,0 +1,6 @@
+# Az eredményeket kiírjuk a fájlba
+        f.write(f'Dobás {i + 1}:\n')
+        f.write(f'  Az első kocka eredménye: {dobk1}\n')  # Első dobás eredménye
+        f.write(f'  A második kocka eredménye: {dobk2}\n')  # Második dobás eredménye
+        f.write(f'  A két dobókocka dobási eredménye: {eredmény}\n\n')  # A két kocka eredménye
+
